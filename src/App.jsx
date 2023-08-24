379a62dcfb336424110ed2d29b59580ca6b44f19
Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>
2023-08-24 15:44:23 +0300

    project has been created

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section
+      className="xl:padding-1 wide:padding-r padding-b
+    "
+    >
+      Hero
+    </section>
+    <section className="padding">
+      Popular Product
+    </section>
+    <section className="padding">
+      Super Quality
+    </section>
+    <section className="padding">
+      Popular Product
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      Special Offer
+    </section>
+    <section className="padding bg-pale-blue">
+      customer reviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      Subscribe
+    </section>
+    <section className="padding-x bg-black padding-t ">
+      Footer
+    </section>
+    
+  </main>
+);
+
+export default App;
