c73869d176a2b6546bc9dec0293287c89c8003c3
Sacettin Şahin <93089940+sacettinsahin@users.noreply.github.com>
2023-08-25 11:08:49 +0300

    shoe card was fixes.

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,19 +1,28 @@
-const ShoeCard = ({ imgUrl, bigShoeImg, changeBigShoeImage }) => {
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
   const handleClick = () => {
-    if (bigShoeImg !== imgUrl.bigShoe) {
-      changeBigShoeImage(imgUrl.bigShoe);
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
     }
   };
+
   return (
     <div
-      className={`border-2 rounded-xl  ${
-        bigShoeImg === imgUrl.bigShoe ? "border-carol-red" : "border-transparent"
+      className={`border-2 rounded-xl ${
+        bigShoeImg === imgURL.bigShoe
+          ? "border-coral-red"
+          : "border-transparent"
       } cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
     >
-        <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:pt-4">
-            <img src={imgUrl.thumbnail} alt="shoe collection" width={127} height={103} className="object-contain"/>
-        </div>
+      <div className='flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4'>
+        <img
+          src={imgURL.thumbnail}
+          alt='shoe colletion'
+          width={127}
+          height={103.34}
+          className='object-contain'
+        />
+      </div>
     </div>
   );
 };
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,57 +1,74 @@
-import { arrowRight } from "../assets/icons"
+import { useState } from "react";
+import {bigShoe1} from "../assets/images"
+import {arrowRight} from "../assets/icons"
 import Button from "../components/Button"
-import {shoes, statistics} from "../constants/index"
-import {bigShoe1}  from "../assets/images/"
-import ShoeCard from "../components/ShoeCard"
-import { useState } from "react"
+import {statistics, shoes} from "../constants/index"
+import ShoeCard from "../components/ShoeCard";
+
 
 const Hero = () => {
-
-  const [bigShoImg, setbigShoImg] = useState(bigShoe1)
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
 
   return (
-    <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container p-2">
-    {/* Left Side */}
-    <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-      <p className="text-xl font-montserrat text-coral-red">
-        Our Summer Collection
-      </p>
-      <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-        <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
-        <br/>
-        <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
-      </h1>
-      <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Distocer stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
-      <Button label="Show Now" icon={arrowRight} />
+    <section
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+        <p className='text-xl font-montserrat text-coral-red'>
+          Our Summer collections
+        </p>
 
-      <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-        {statistics.map((stat)=>(
-          <div key={stat.label}>
-            <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-            <p className="leading-7 text-slate-gray font-montserrat">{stat.label}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival
+          </span>
+          <br />
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+        </h1>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
 
-        {/* Right Side */}
-    <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
-      <img src={bigShoe1} alt="shoe collection" width={610} height={500} className="object-contain relative z-10"/>
+        <Button label='Shop now' iconURL={arrowRight} />
 
-      <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-        {
-          shoes.map((shoe)=>(
-            <div key={shoe}>
-              <ShoeCard imgUrl={shoe} changeBigShoeImage={(shoe)=>{setbigShoImg(shoe)}} bigShoeImg={bigShoImg} />
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map((stat, index) => (
+            <div key={index}>
+              <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+              <p className='leading-7 font-montserrat text-slate-gray'>
+                {stat.label}
+              </p>
             </div>
-          ))
-        }
+          ))}
+        </div>
       </div>
-    </div>  
 
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
+        <img
+          src={bigShoeImg}
+          alt='shoe colletion'
+          width={610}
+          height={502}
+          className='object-contain relative z-10'
+        />
 
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+          {shoes.map((image, index) => (
+            <div key={index}>
+              <ShoeCard
+                index={index}
+                imgURL={image}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
+                bigShoeImg={bigShoeImg}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
